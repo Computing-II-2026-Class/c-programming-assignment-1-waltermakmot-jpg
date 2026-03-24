18c9d775581de63d913b3d98eb1e2120a242bc64
waltermakmot-jpg <waltermakmot@gmail.com>
2026-03-24 20:59:06 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,31 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Makmot Walter */
+/* Student Number: U/2026/BIE/05296/PE */
 
 #include <stdio.h>
+int main () {
 
-int main(void)
-{
-    /* Write your code here */
+float unitsConsumed, costPerunit, totalElectricityBill;
+    // Prompt the user for the units consumed and the cost per unit
+
+
+    printf("Enter the units consumed: ");
+    scanf("%f", &unitsConsumed);
+    printf("Enter the cost per unit: ");
+    scanf("%f", &costPerunit);
+
+    // Calculate the total electricity bill
+
+    totalElectricityBill = unitsConsumed * costPerunit;
+
+    // Display the total electricity bill
+
+    printf("The total electricity bill is: %.2f UGX\n", totalElectricityBill);
+    printf("..........Electricity Bill Details..........\n");
+    printf("The units consumed are: %.2f\n", unitsConsumed);
+    printf("The cost per unit is: %.2f UGX\n", costPerunit);
+    printf("\nThank you for using our service!");
+
+
 
     return 0;
 }
