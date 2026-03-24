a0dff4646be9e559c80b259aba42b887572b97c8
waltermakmot-jpg <waltermakmot@gmail.com>
2026-03-24 20:45:20 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,19 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name:Makmot Walter */
+/* Student Number:U/2026/BIE/05296/PE*/
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float amount,fee_percent,fee_amount,total_deducted;
+printf("Enter amount to send:");
+scanf("%f",&amount);
+printf("Enter transaction fee(%):");
+scanf("%f",&fee_percent);
+fee_amount = (fee_percent/100)*amount;
+total_deducted =amount+fee_amount;
+printf("Transaction fee:%.2f ugx\n",fee_amount);
+printf("Total deducted:%.2fugx\n",total_deducted);
 
     return 0;
 }
