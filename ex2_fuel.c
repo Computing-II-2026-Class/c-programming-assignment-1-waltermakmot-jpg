9b5b4f97ab571f71ffbe96c2a6001220ca9867be
waltermakmot-jpg <waltermakmot@gmail.com>
2026-03-24 20:54:04 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,14 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Makmot Walter*/
+/*Student Number:  U/2026/BIE/05296/PE */
+# include <stdio.h>
 
-#include <stdio.h>
-
-int main(void)
-{
-    /* Write your code here */
-
+int main () {
+    float distance, fuel, fuel_efficiency;
+    printf("Enter the distance traveled in kilometers: ");
+    scanf("%f", &distance);
+    printf("Enter the amount of fuel consumed in liters: ");
+    scanf("%f", &fuel);
+    fuel_efficiency = distance / fuel;
+    printf("Your fuel efficiency is: %.2f km/l", fuel_efficiency);
     return 0;
 }
